47abc7897ea3f8101484c1c3f1ac13b34285d36b
vikramaditya singh naruka <vikramadityanaruka@gmail.com>
2025-02-12 23:42:03 +0530

    2nd commit

--- src/data/video-links.js
+++ src/data/video-links.js
@@ -1,27 +1,27 @@
  
 export const videoLink = [
-    {
-        title:"Limits for Class 11 and IIT-JEE | Existence of limit | LHL and RHL",
-        description:"Classs 11 Chapter 13 Introduction | What is limit of a function at any number | Existence of limit | Evaluation of Left hand limits and Right hand limits ||",
-        link:"https://www.youtube.com/embed/cpWwUEV9u8U?si=9iSNDORLo1fnTYFK"
+    // {
+    //     title:"Limits for Class 11 and IIT-JEE | Existence of limit | LHL and RHL",
+    //     description:"Classs 11 Chapter 13 Introduction | What is limit of a function at any number | Existence of limit | Evaluation of Left hand limits and Right hand limits ||",
+    //     link:"https://www.youtube.com/embed/cpWwUEV9u8U?si=9iSNDORLo1fnTYFK"
 
-    },
-    {
-        title:"CUBIC EQUATION FACTORIZATION SHORTCUT| SOLVING CUBIC EQUATIONS IN 10 SECONDS ||",
-        description:"SOLVE CUBIC EQUATIONS IN 10 SECONDS | Cubic Equation Tricks By Shobhit Sir. Learn Cubic Equation  Tricks For All Competitive Exams With Bindhyachal Academy",
-        link:"https://www.youtube.com/embed/t63cp8arc_Q?si=63wUOjVzDyxhpQQp"
+    // },
+    // {
+    //     title:"CUBIC EQUATION FACTORIZATION SHORTCUT| SOLVING CUBIC EQUATIONS IN 10 SECONDS ||",
+    //     description:"SOLVE CUBIC EQUATIONS IN 10 SECONDS | Cubic Equation Tricks By Shobhit Sir. Learn Cubic Equation  Tricks For All Competitive Exams With Bindhyachal Academy",
+    //     link:"https://www.youtube.com/embed/t63cp8arc_Q?si=63wUOjVzDyxhpQQp"
 
-    },
-    {
-        title:"Solve any Quadratic Equation In 10 Secs | Tricks for roots of a quadratic equation",
-        description:"Shortcut trick | SOLVE QUADRATIC EQUATIONS IN 10 SECONDS | Quadratic Equation Tricks By Shobhit Sir. Learn Quadratic Equation  Tricks For All Competitive Exams With Bindhyachal Academy",
-        link:"https://www.youtube.com/embed/odeL5P7i50s?si=xdovtOsF76go1j9W"
+    // },
+    // {
+    //     title:"Solve any Quadratic Equation In 10 Secs | Tricks for roots of a quadratic equation",
+    //     description:"Shortcut trick | SOLVE QUADRATIC EQUATIONS IN 10 SECONDS | Quadratic Equation Tricks By Shobhit Sir. Learn Quadratic Equation  Tricks For All Competitive Exams With Bindhyachal Academy",
+    //     link:"https://www.youtube.com/embed/odeL5P7i50s?si=xdovtOsF76go1j9W"
 
-    },
-    {
-        title:"Class 10th||Maths||CIRCLE || Products of lengths of segments of two chords",
-        description:"We are Bindhyachal Academy providing online and offline classes for IIT-Jee, Foundation/Pre-foundation courses and academics as well.",
-        link:"https://www.youtube.com/embed/q0HdpSd6WAU?si=CeD2MwXejMmwnIC2"
+    // },
+    // {
+    //     title:"Class 10th||Maths||CIRCLE || Products of lengths of segments of two chords",
+    //     description:"We are Bindhyachal Academy providing online and offline classes for IIT-Jee, Foundation/Pre-foundation courses and academics as well.",
+    //     link:"https://www.youtube.com/embed/q0HdpSd6WAU?si=CeD2MwXejMmwnIC2"
 
-    }
+    // }
 ]
